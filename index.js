75226c65e02b1a2d45c77b2c3bfb663bb95ac2cb
Colin <owens106@purde.edu>
2018-05-14 16:19:22 -0400

    chagned paragraph text when button is clicked

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const button = document.querySelector('button.greeting')
 function changeText(){
-    alert('yeah')
+    const para = document.querySelector('p.greeting')
+    para.textContent = "changed text" //sets the text to a custom message
+    //para.textContent w/o an assignment will return the current content
 }
 
 button.addEventListener('click',changeText)
