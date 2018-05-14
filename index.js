1291522ca54f9a9327e2f2dabad06b463edf8ae2
Colin <owens106@purde.edu>
2018-05-14 16:33:44 -0400

    changed button text with changeText

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const button = document.querySelector('button.greeting')
-function changeText(){
+const count =0
+function changeText(ev){
+    ev.target.textContent="you just clicked me" + count +" times"
     const para = document.querySelector('p.greeting')
     para.textContent = "changed text" //sets the text to a custom message
     //para.textContent w/o an assignment will return the current content
 }
-
 button.addEventListener('click',changeText)
+//button.addEventListener('mouseover',changeText)
