39fea828407ca26542fc96ad11ffdc53ecb3857f
Colin <owens106@purde.edu>
2018-05-14 17:37:37 -0400

    add a 2nd header, change a button to modify first header, add 2nd button to modifty 2nd header, add a form to custom change the first header

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
-const button = document.querySelector('button.greeting')
-const count =0
-function changeText(ev){
-    ev.target.textContent="you just clicked me" + count +" times"
-    const para = document.querySelector('p.greeting')
-    para.textContent = "changed text" //sets the text to a custom message
-    //para.textContent w/o an assignment will return the current content
+const clickTrack = document.querySelector('button.greeting')
+const H1Change = document.querySelector('button.headChange')
+const H2Change = document.querySelector('button.head2Change')
+const form = document.querySelector('form')
+ 
+let count =1
+function counter(ev){
+    ev.target.textContent="you just clicked me " + count +" times"
+    count++
 }
-button.addEventListener('click',changeText)
-//button.addEventListener('mouseover',changeText)
+function H1Change_(){
+    const H1 = document.querySelector('h1.h1_0')
+    H1.textContent="You just Changed the first header"
+}
+function H2Change_(){
+    const H2= document.querySelector('h1.h1_1')
+    H2.textContent="You just Changed The second Header"
+}
+function H1FormChange(){
+    let input = document.getElementById("input").value
+    console.log("test")
+    const H1 = document.querySelector('h1.h1_0')
+    H1.textContent=input
+    return false
+}
+clickTrack.addEventListener('click',counter)
+H1Change.addEventListener('click',H1Change_)
+H2Change.addEventListener('click',H2Change_)
+//form.addEventListener('submit',H1FormChange)
