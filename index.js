0d23aaf0d4366f77e4465692a8c063835188bb68
Colin <owens106@purde.edu>
2018-05-15 15:30:24 -0400

    append the user as a DOM element to the users div

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const form = document.querySelector('#userForm')
+
  
 const handleSubmit = function(ev) {
   ev.preventDefault()
@@ -6,11 +7,10 @@ const handleSubmit = function(ev) {
   const f = ev.target
   const userName = f.userName.value
   const age = f.age.value
-  //users.textContent += ' ' + userName
-  users.innerHTML += `<p>${userName}, ${age} </p>` //adds each into own paragraph
-
-  //f.userName.value = ''
-  //f.age.value=''
+  //users.innerHTML += `<p>${userName}, ${age} </p>` //adds each into own paragraph
+  const p=document.createElement('p')
+  p.textContent = `${userName}, ${age}`
+  users.appendChild(p) //adds p to the end of users
   //dont reset each field use form.reset(). doesnt reset cursor
   f.reset()
   f.userName.focus()//sets focus to certain field
