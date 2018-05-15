d9a5530f2a44caf747481979fc70335f10803e38
Colin <owens106@purde.edu>
2018-05-15 15:09:42 -0400

    add an age field, format it with username, clear age field on submit

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ const handleSubmit = function(ev) {
   const users = document.querySelector('#users')
   const f = ev.target
   const userName = f.userName.value
+  const age = f.age.value
   //users.textContent += ' ' + userName
-  users.innerHTML += '<p>' + userName + '</p>'
+  users.innerHTML += '<p>' + userName + ", "+age+'</p>' //adds each into own paragraph
 
   f.userName.value = ''
+  f.age.value=''
 }
 
 form.addEventListener('submit', handleSubmit)
